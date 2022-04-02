0122dcd059a387e309e48940533366a2964eeed7
DarthTuna <david.sochacki@googlemail.com>
2022-04-02 18:47:44 +0200

    Updated some dependencies; Removed redundand list clearing; Added a few files to .gitignore; Revamped deprecated code; Messed up records will be ignored;

--- src/main/java/de/darthtuna/gw2/handelsposten/util/CSVHelper.java
+++ src/main/java/de/darthtuna/gw2/handelsposten/util/CSVHelper.java
@@ -20,33 +20,39 @@ public class CSVHelper
     public List<TableEntry> getFile()
     {
         List<TableEntry> entryList = new ArrayList<>();
-        entryList.clear();
         try (var inputStream = new URL("http://www.gw2spidy.com/api/v0.9/csv/all-items/all").openStream();
-                var reader = new InputStreamReader(inputStream))
+             var reader = new InputStreamReader(inputStream))
         {
-            var parser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withQuote('%'));
+            var builder = CSVFormat.Builder.create(CSVFormat.DEFAULT).setQuote('%').setHeader().setSkipHeaderRecord(true);
+            var parser = new CSVParser(reader, builder.build());
             for (var record : parser)
             {
-                int offerAvailability = Integer.parseInt(record.get("offer_availability"));
-                int saleAvailability = Integer.parseInt(record.get("sale_availability"));
-                if (offerAvailability < 10_000 || saleAvailability < 10_000)
+                try
                 {
-                    // Wenn ein Item kaum gehandelt wird, lohnt es sich nicht, diese aufzukaufen, weil kein interesse
-                    // daran besteht.
-                    continue;
+                    int offerAvailability = Integer.parseInt(record.get("offer_availability"));
+                    int saleAvailability = Integer.parseInt(record.get("sale_availability"));
+                    if (offerAvailability < 10_000 || saleAvailability < 10_000)
+                    {
+                        // Wenn ein Item kaum gehandelt wird, lohnt es sich nicht, diese aufzukaufen, weil kein interesse
+                        // daran besteht.
+                        continue;
+                    }
+                    var entry = new TableEntry();
+                    entry.setOfferAvailability(offerAvailability);
+                    entry.setSaleAvailability(saleAvailability);
+                    entry.setName(record.get("name"));
+                    entry.setMaxOfferPrice(Integer.parseInt(record.get("max_offer_unit_price")));
+                    entry.setMinSaleUnitPrice(Integer.parseInt(record.get("min_sale_unit_price")));
+                    if (entry.getMargin() < 1)
+                        continue;
+                    entryList.add(entry);
                 }
-                var entry = new TableEntry();
-                entry.setOfferAvailability(offerAvailability);
-                entry.setSaleAvailability(saleAvailability);
-                entry.setName(record.get("name"));
-                entry.setMaxOfferPrice(Integer.parseInt(record.get("max_offer_unit_price")));
-                entry.setMinSaleUnitPrice(Integer.parseInt(record.get("min_sale_unit_price")));
-                if (entry.getMargin() < 1)
-                    continue;
-                entryList.add(entry);
+                catch (NumberFormatException e)
+                {
+                    logger.error("Found a messed up record: {}. Skipping it.", record);
+                }
             }
-        }
-        catch (IOException e)
+        } catch (IOException e)
         {
             logger.catching(e);
         }
